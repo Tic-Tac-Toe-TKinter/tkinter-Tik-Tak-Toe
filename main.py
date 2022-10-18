e38a46b2eb88f42bad4f894ebdfa3a7cda32b611
Olelksandr Lisnytskiy <oleksandrl1009@gmail.com>
2022-10-18 11:07:34 -0700

    defense bot ready

--- main.py
+++ main.py
@@ -12,28 +12,45 @@ def bot(lst1: list[list[int]], val: int) -> list[list[int]]:
     :param val: value to play for as computer
     :return: list with added value(val) to any row or column
     """
+    opposite_value = 1 if val == 2 else 2
     # check if there are two values of other player in this row, if yes place other third value
     for i in range(len(lst1)):
-        if sum(lst1[i]) == (4 // (4 - val)) * 2:
+        if sum(lst1[i]) == opposite_value * 2 and lst1[i].count(0) == 1:
             lst1[i][lst1[i].index(0)] = val
+            print("two values in a row")
             return lst1
 
     # check if there are two values of other player in this column, if yes place other third value
     for i in range(len(lst1)):
-        if sum(lst1[0][i], lst1[1][i], lst1[2][i]) == (4 // (4 - val)) * 2:
-            for j in range(2):
+        if sum([lst1[0][i], lst1[1][i], lst1[2][i]]) == opposite_value * 2:
+            for j in range(3):
                 if lst1[j][i] == 0:
                     lst1[j][i] = val
+                    print("two values in a column")
                     return lst1
 
+    # check if there are two values of other player on the cross lines, if yes place other third value
+    if sum([lst1[0][0], lst1[1][1], lst1[2][2]]) == opposite_value * 2 and 0 in [lst1[0][0], lst1[1][1], lst1[2][2]]:
+        for i in range(3):
+            if lst1[i][i] == 0:
+                lst1[i][i] = val
+                print("two values in a line left right")
+
+                return lst1
+    elif sum([lst1[0][2], lst1[1][1], lst1[2][0]]) == opposite_value * 2 and 0 in [lst1[0][0], lst1[1][1], lst1[2][2]]:
+        for i in range(3):
+            if lst1[i][2-i] == 0:
+                lst1[i][2-i] = val
+                print("two values in a line right left")
+                return lst1
 
     # place val to random place in this row if it is empty
     if sum(lst1[0]) == 0:
-        lst1[0][randint([0, 1, 2])] = val
+        lst1[0][randint(0, 2)] = val
     elif sum(lst1[1]) == 0:
-        lst1[1][randint([0, 1, 2])] = val
+        lst1[1][randint(0, 2)] = val
     elif sum(lst1[2]) == 0:
-        lst1[2][randint([0, 1, 2])] = val
+        lst1[2][randint(0, 2)] = val
     return lst1
 
 
