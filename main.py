356f98d108ce5c714f6b23850add4f381e4be5c6
Olelksandr Lisnytskiy <oleksandrl1009@gmail.com>
2022-10-23 00:19:42 -0700

    Bot works perfectly

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from tkinter import Tk, Label, Button, Frame
+from tkinter import Tk, Button, PhotoImage
 from tkinter.messagebox import showinfo
 import tkinter.font as font
-from tkinter import *
 from random import randint
-from PIL import ImageTk, Image
 from sys import exit
 
 root = Tk()
-
 
 
 def changer(value: int, row_1: int, column_1: int, font_size: font) -> None:
@@ -31,7 +28,7 @@ def changer(value: int, row_1: int, column_1: int, font_size: font) -> None:
         button_to_change.grid(row=row_1, column=column_1)
 
 
-def bot_second_level(lst1: list[list[int]], userValue: int, font_size: font) -> None:
+def bot_second_level(lst1: list[list[int]], userValue: int, font_size: font) -> int | None:
     """
     Function for player to play against computer with easy level. Input argument is list of lists of integers
     that represents all X: 1 and O: -1 on the board. It returns the list with new value of val
@@ -42,59 +39,59 @@ def bot_second_level(lst1: list[list[int]], userValue: int, font_size: font) -> 
     :return: list with added value(val) to any row or column
     """
     botValue = userValue * -1
+
+    # Check if all cells is occupied, if yes exit program
+    if all([all(x) for x in game]):
+        return None
     # check if there are two values of other player in this row, if yes place other third value
     for i in range(len(lst1)):
         if sum(lst1[i]) == userValue * 2 and lst1[i].count(0) == 1:
-            print("Two in a row")
             changer(botValue, i, lst1[i].index(0), font_size)
             lst1[i][lst1[i].index(0)] = botValue
 
-            return None
+            return 1
 
     # check if there are two values of other player in this column, if yes place other third value
     for i in range(len(lst1)):
         if sum([lst1[0][i], lst1[1][i], lst1[2][i]]) == userValue * 2:
-            print("Two in a column")
             for j in range(3):
                 if lst1[j][i] == 0:
                     changer(botValue, j, i, font_size)
                     lst1[j][i] = botValue
 
-                    return None
+                    return 1
 
     # check if there are two values of other player on the cross lines, if yes place other third value
     if sum([lst1[0][0], lst1[1][1], lst1[2][2]]) == userValue * 2 and 0 in [lst1[0][0], lst1[1][1], lst1[2][2]]:
         for i in range(3):
             if lst1[i][i] == 0:
-                print("Two in a line lef right")
                 changer(botValue, i, i, font_size)
                 lst1[i][i] = botValue
 
-                return None
+                return 1
 
     elif sum([lst1[0][2], lst1[1][1], lst1[2][0]]) == userValue * 2 and 0 in [lst1[0][0], lst1[1][1], lst1[2][2]]:
         for i in range(3):
             if lst1[i][2 - i] == 0:
-                print("Two in a line right to left")
                 changer(botValue, i, 2 - i, font_size)
                 lst1[i][2 - i] = botValue
 
-                return None
+                return 1
 
     # place val to random place in this row if it is empty
     randVal = randint(0, 2)
     if sum(lst1[0]) == 0:
         lst1[0][randVal] = botValue
         changer(botValue, 0, randVal, font_size)
-        return None
+        return 1
     elif sum(lst1[1]) == 0:
         lst1[1][randVal] = botValue
         changer(botValue, 1, randVal, font_size)
-        return None
+        return 1
     elif sum(lst1[2]) == 0:
         lst1[2][randVal] = botValue
         changer(botValue, 0, randVal, font_size)
-        return None
+        return 1
 
     while True:
         x = randint(0, 2)
@@ -102,14 +99,12 @@ def bot_second_level(lst1: list[list[int]], userValue: int, font_size: font) -> 
         if lst1[x][y] != 0:
             continue
         else:
-            print("Random value")
             lst1[x][y] = botValue
             changer(botValue, x, y, font_size)
-            break
-    return None
+            return 1
 
 
-def bot_first_level(lst1: list[list[int]], userValue: int, font_size: font) -> None:
+def bot_first_level(lst1: list[list[int]], userValue: int, font_size: font) -> int | None:
     """
     Function for player to play against computer with easy level. Input argument is list of lists of integers
     that represents all X: 1 and O: 2 on the board. It returns the list with new value of val
@@ -122,6 +117,8 @@ def bot_first_level(lst1: list[list[int]], userValue: int, font_size: font) -> N
     botValue = 1 if userValue == -1 else -1
 
     while True:
+        if all([all(i) for i in game]):
+            return None
         x = randint(0, 2)
         y = randint(0, 2)
         if lst1[x][y] != 0:
@@ -130,6 +127,7 @@ def bot_first_level(lst1: list[list[int]], userValue: int, font_size: font) -> N
             lst1[x][y] = botValue
             changer(botValue, x, y, font_size)
             break
+    return 1
 
 
 def win_check() -> None:
@@ -140,12 +138,12 @@ def win_check() -> None:
     """
     for k in range(3):
         if game[0][k] + game[1][k] + game[2][k] == 3:
-            showinfo("X - won")
+            showinfo(message="X - won")
             print("X - won")
             exit(1)
             break
         elif game[0][k] + game[1][k] + game[2][k] == -3:
-            showinfo("O - won")
+            showinfo(message="O - won")
             print("O - won")
             exit(1)
             break
@@ -153,7 +151,7 @@ def win_check() -> None:
     for i in game:  # Checking for rows and diagonals
         if i[0] + i[1] + i[2] == 3 or game[0][0] + game[1][1] + game[2][2] == 3 or game[0][2] + game[1][1] + game[2][
             0] == 3:
-            showinfo("X - won")
+            showinfo(message="X - won")
             print("X - won")
             exit(1)
             break
@@ -180,14 +178,14 @@ if __name__ == '__main__':
     # label = root.Label(background="black")
     turn = 1
     myFont = font.Font(size=20)
-    gameMode = 'Bot'  # input("Choose the game mode to play(Bot/Player): ")
+    gameMode = 'bot'  # input("Choose the game mode to play(Bot/Player): ")
 
-    if gameMode == "Bot":
+    if gameMode == "bot":
         difficultyLevel = 2  # input("choose difficulty level(1/2): ")
     for r in range(3):
         for c in range(3):
             match gameMode.lower():
-                case "layer":
+                case "player":
                     def tkk(x=labels[r][c]):
                         global game
                         global turn
@@ -227,18 +225,23 @@ if __name__ == '__main__':
                         game[row_1][
                             column_1] = turn  # connecting the number of the button to the element in the list of lists
 
-                        cell = str(row_1) + str(column_1)
-
                         changer(turn, row_1, column_1, myFont)
 
-                        if difficultyLevel == '1':
-                            bot_first_level(game, turn, myFont)
-                        else:
-                            bot_second_level(game, turn, myFont)
+                        if difficultyLevel == 1:
 
-                        print(cell)
-                        print(game)
+                            if bot_first_level(game, turn, myFont) is None and win_check() is None:
+                                showinfo(message="Draw")
+                                exit(1)
+                        else:
+
+                            if bot_second_level(game, turn, myFont) is None and win_check() is None:
+                                showinfo(message="Draw")
+                                exit(1)
                         win_check()
+                        # cell = str(row_1) + str(column_1)
+                        # print(cell)
+                        # print(game)
+
             button = Button(root, width=10, height=5, padx=10,
                             text=labels[r][c], command=tkk)
             button['font'] = myFont
